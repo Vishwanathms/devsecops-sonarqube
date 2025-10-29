3cf447e0ac8d799b601815b7c4c2dc22f23c5589
Vishwanathms <vishwanath.murthy@gmail.com>
2025-10-29 11:40:24 +0530

    new org and new project

--- python_app/redis_client.py
+++ python_app/redis_client.py
@@ -5,12 +5,14 @@ def get_redis_client():
     redis_host = "localhost"
     redis_port = 6379
     redis_password = "Password123!"  # Sonar will flag this
+    rds_pwd = "Password123!"  # Sonar will flag this too
     
     try:
         client = redis.Redis(
             host=redis_host,
             port=redis_port,
             password=redis_password,
+            rds_pwd=rds_pwd,
             decode_responses=True
         )
         client.ping()  # Force connection
